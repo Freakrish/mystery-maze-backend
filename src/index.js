f2406a5e591b1ae61b77f9f4c5f3b00ba9391856
priydarshi12 <21001016054.jcboseust.ac.in>
2024-07-01 13:55:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { scoreRouter } from "./routes/score.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT ||8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/result", scoreRouter);
+
+
+
+mongoose
+  .connect(process.env.DATABASE_URL, {})
+  .then(() => console.log("DB connected successfully"))
+  .catch((error) => console.log("error in db connection", error));
+
+  app.listen(PORT,()=>{console.log("server is live")})
